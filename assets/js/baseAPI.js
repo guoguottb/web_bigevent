b52091bc8568bc7c9246dc5e75351e058493e220
gggtongbiao <1223240438@qq.com>
2022-06-09 20:10:00 +0800

    完成了渲染头像和用户名|账号退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,8 @@
 $.ajaxPrefilter((options) => {
   options.url = "http://www.liulongbin.top:3007" + options.url;
+  if (options.url.includes("/my/")) {
+    options.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+function getUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    success: function (res) {
+      if (res.status !== 0) {
+        location.href = "/login.html";
+        return layer.msg(res.message);
+      } else {
+        renderAvatar(res.data);
+      }
+    },
+  });
+}
+//获取用户信息
+getUserInfo();
+//渲染头像
+function renderAvatar(data) {
+  const name = data.nickname || data.username;
+  $("#welcome").html(`欢迎 ${name}`);
+  if (data.user_pic !== null) {
+    $(".layui-nav-img").attr("src", data.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    let first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+    $(".layui-nav-img").hide();
+  }
+}
+//退出登录
+$("#btnLogout").on("click", function () {
+  layer.confirm("是否确认退出?", { icon: 3, title: "提示" }, function (index) {
+    //1.清除本地的token
+    localStorage.removeItem("token");
+    //2.跳转到login页面
+    location.href = "/login.html";
+    //do something
+    // layer.close(index);
+  });
+});
